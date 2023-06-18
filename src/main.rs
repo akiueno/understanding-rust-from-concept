3fece2aed5e1ea93f1ec084092c7ff7559322dbd
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-18 17:19:11 +0900

    6.2

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,26 @@
 use std::fs::File;
 use std::io::Read;
 
-const BUFSIZE: usize = 1024;
-
 fn main() -> std::io::Result<()> {
     let mut f = File::open("input.txt")?;
-    let mut buf = [0_u8; BUFSIZE];
 
     let mut lines = Vec::new();
     let mut linebuf = String::new();
 
-    loop {
-        let read_size = f.read(&mut buf)?;
+    let mut buf = Vec::new();
+    let read_size = f.read_to_end(&mut buf)?;
 
-        println!("buf: {:?}", &buf);
-        println!("read_size: {:?}", read_size);
+    println!("read_size: {}", read_size);
 
-        if read_size == 0 {
-            break;
-        }
-
-        for cc in &buf[..read_size] {
-            if *cc == b'\n' {
-                lines.push(linebuf);
-                linebuf = String::new();
-            } else {
-                linebuf.push(*cc as char);
-            }
+    for cc in &buf[..read_size] {
+        if *cc == b'\n' {
+            lines.push(linebuf.clone());
+            linebuf = String::new();
+        } else {
+            linebuf.push(*cc as char);
         }
     }
-
-    println!("{:?}", lines);
+    println!("lines: {:?}", lines);
 
     Ok(())
 }
